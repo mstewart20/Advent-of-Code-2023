a1084a7e573b9031e856b9458b0e4d38aef9c294
mstewart20 <mitchellstewart@gmail.com>
2023-12-30 20:12:41 +0000

    Part 1 of Day 1

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,2 +1,24 @@
-x = 1+1
-print("hello world" + str(x))
+# Find a digit in a string
+def findDigit(s):
+  for char in s:
+    if char.isdigit():
+      return char
+  return ""
+
+def findNumber(s):
+  x = findDigit(s)
+  y = findDigit(s[::-1])
+
+  return int(x + y)
+
+
+# Using readlines()
+inputFile = open('Day 1/input.txt', 'r')
+lines = inputFile.readlines()
+
+sum = 0
+
+for line in lines:
+  sum += findNumber(line)
+
+print("Sum is: " + str(sum))
